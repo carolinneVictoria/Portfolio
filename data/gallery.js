2e107f713523fefc07ae1e79dffeed90eec8bd69
Carolinne Victoria <carolinevictoria1224@gmail.com>
2026-08-20 11:00:32 -0300

    feat: add gallery data for artworks; include descriptions and images

---
+++ data/gallery.js
@@ -0,0 +1,22 @@
+// Dados da galeria "Além do código".
+// Para adicionar uma ilustração real: coloque o arquivo em images/art/ e crie uma entrada aqui.
+export const artworks = [
+    {
+        id: 'art-1',
+        image: 'images/art/tayuna.png',
+        title: 'Tayuna',
+        description: 'Garota indigena',
+    },
+    {
+        id: 'art-2',
+        image: 'images/art/siren.png',
+        title: 'Sereia',
+        description: 'Sereia misteriosa.',
+    },
+    {
+        id: 'art-3',
+        image: 'images/art/princess.png',
+        title: 'Princesa e Cavaleiro',
+        description: 'Princesa e cavaleiro.',
+    },
+];

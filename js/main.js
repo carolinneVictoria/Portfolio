f43ffae83b4b677ea85417c86ba7a322eb3d6558
Carolinne Victoria <carolinevictoria1224@gmail.com>
2026-08-20 11:01:15 -0300

    feat: add main JavaScript file to initialize application components

---
+++ js/main.js
@@ -0,0 +1,17 @@
+import { initThemeToggle } from './theme.js';
+import { initMobileMenu, initScrollSpy } from './navbar.js';
+import { initProjects } from './projects.js';
+import { initStack } from './stack.js';
+import { initGallery } from './gallery.js';
+import { initLab } from './lab.js';
+import { initContactForm, initCopyEmail } from './contact.js';
+
+initThemeToggle();
+initMobileMenu();
+initScrollSpy();
+initProjects();
+initStack();
+initGallery();
+initLab();
+initContactForm();
+initCopyEmail();
